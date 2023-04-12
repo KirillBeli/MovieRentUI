d3cba43ebeec818082ac763f4eeb8433cca3892a
KirillBeli <k0546353307@gmail.com>
2023-04-12 13:12:08 +0100

    Initial Commit

---
+++ MovieRentUI/MovieRentUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRentUIApp.swift
+//  MovieRentUI
+//
+//  Created by Kyrylo Bielykov on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieRentUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
